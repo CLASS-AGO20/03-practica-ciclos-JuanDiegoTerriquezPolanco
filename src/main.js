f3b9037439216d691a0115955c9ba8429473a817
JuanDiegoTerriquezPolanco <jterriquez0@ucol.mx>
2020-12-09 16:01:12 -0600

    implemented factorial

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
-export default class App {}
+export default class App {    
+    factorial(numero){
+        let resultado=1;
+        for(let i=numero;i>0;i--){
+            resultado=resultado*i;
+            console.log(resultado);
+        }
+        return resultado;
+    }
+}
 
 let app = new App();
+app.factorial(5);
