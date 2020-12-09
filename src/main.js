71c395d941b19598ca6fd2d70dbb51913ecf89da
JuanDiegoTerriquezPolanco <jterriquez0@ucol.mx>
2020-12-09 16:29:22 -0600

    implemented obtenerDivisibles

--- src/main.js
+++ src/main.js
@@ -16,8 +16,20 @@ export default class App {
         }
         return cadena;
     }
+    obtenerDivisibles(numero){
+        let i=0,aux=1;
+        do {
+            if(numero%i==0){
+                aux++;
+            }
+            i++
+            console.log(aux)
+        } while (i<numero);
+        return aux;
+    }
 }
 
 let app = new App();
 app.factorial(5);
 app.convertirAString(5);
+app.obtenerDivisibles(6);
